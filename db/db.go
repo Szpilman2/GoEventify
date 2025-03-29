f1e1afb77707c00dada7892d73952275c6492960
Peyman Shabani <peymanshabani1998@gmail.com>
2025-03-29 12:55:34 +0330

    add database to project and initialize it

---
+++ db/db.go
@@ -0,0 +1,23 @@
+package db
+
+import (
+	"database/sql"
+
+	_ "github.com/mattn/go-sqlite3"  //we add _ to prevent removing this package. because we don't use it directly and we use database/sql instead.
+)
+
+var DB *sql.DB
+
+func InitDB() {
+	var err error
+	DB, err := sql.Open("sqlite3", "api.db")  //open a connection to database.
+
+	if err != nil {
+		panic("could not connect to database.")
+	}
+	DB.SetMaxOpenConns(10)
+
+	DB.SetMaxIdleConns(5)
+
+    // createTables()
+}
