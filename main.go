4b617526c9d058917169e9787e4ee56fd55e3047
Peyman Shabani <peymanshabani1998@gmail.com>
2025-03-29 11:49:46 +0330

    add gin to project and handle simple endpoint

--- main.go
+++ main.go
@@ -2,10 +2,25 @@ package main
 
 import (
 	"fmt"
+	"net/http"
 
-	// "github.com/gin-gonic/gin"
+	"github.com/gin-gonic/gin"
 )
 
 func main(){
-	fmt.Println("Hello world!")
+	asciArt := `
+	██████╗  ██████╗ ███████╗██╗   ██╗███████╗███╗   ██╗████████╗██╗███████╗██╗   ██╗
+	██╔════╝ ██╔═══██╗██╔════╝██║   ██║██╔════╝████╗  ██║╚══██╔══╝██║██╔════╝╚██╗ ██╔╝
+	██║  ███╗██║   ██║█████╗  ██║   ██║█████╗  ██╔██╗ ██║   ██║   ██║█████╗   ╚████╔╝ 
+	██║   ██║██║   ██║██╔══╝  ╚██╗ ██╔╝██╔══╝  ██║╚██╗██║   ██║   ██║██╔══╝    ╚██╔╝  
+	╚██████╔╝╚██████╔╝███████╗ ╚████╔╝ ███████╗██║ ╚████║   ██║   ██║██║        ██║   
+	╚═════╝  ╚═════╝ ╚══════╝  ╚═══╝  ╚══════╝╚═╝  ╚═══╝   ╚═╝   ╚═╝╚═╝        ╚═╝   
+	`
+	fmt.Println(asciArt)
+
+	server := gin.Default()  // default returns an http engine instance with the logger and recovery middleware already attached.
+	server.GET("/events", func (context *gin.Context)  {
+		context.JSON(http.StatusOK, gin.H{"message":"Hello World!"})   //gin.H is a map (dictionary)
+	})
+	server.Run(":8080") //start listening to incoming requests to localhost:8080
 }
