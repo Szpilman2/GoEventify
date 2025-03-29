2f6b64d2897ade7c9be72c79a8666626041d122f
Peyman Shabani <peymanshabani1998@gmail.com>
2025-03-29 17:04:27 +0330

    fix some errors in database code

--- db/db.go
+++ db/db.go
@@ -2,15 +2,16 @@ package db
 
 import (
 	"database/sql"
+	"log"
 
-	_ "github.com/mattn/go-sqlite3"  //we add _ to prevent removing this package. because we don't use it directly and we use database/sql instead.
+	_ "github.com/mattn/go-sqlite3" //we add _ to prevent removing this package. because we don't use it directly and we use database/sql instead.
 )
 
 var DB *sql.DB
 
 func InitDB() {
 	var err error
-	DB, err := sql.Open("sqlite3", "api.db")  //open a connection to database.
+	DB, err = sql.Open("sqlite3", "api.db") //open a connection to database.
 
 	if err != nil {
 		panic("could not connect to database.")
@@ -19,5 +20,21 @@ func InitDB() {
 
 	DB.SetMaxIdleConns(5)
 
-    // createTables()
+	createTables()
+}
+
+func createTables() {
+	createEventsTable := `
+		CREATE TABLE IF NOT EXISTS events (
+			id INTEGER PRIMARY KEY AUTOINCREMENT,
+			name  TEXT NOT NULL,
+			description TEXT NOT NULL,
+			dateTime DATETIME NOT NULL,
+			user_id INTEGER
+		)
+	`
+	_, err := DB.Exec(createEventsTable)
+	if err != nil {
+		log.Fatalf("could not create events table %v", err)
+	}
 }
