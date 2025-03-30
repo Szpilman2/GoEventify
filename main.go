bd4c83abf04f679a82316734bbf6434d04b137f5
Peyman Shabani <peymanshabani1998@gmail.com>
2025-03-30 23:24:49 +0330

    refactor main.go

--- main.go
+++ main.go
@@ -3,10 +3,7 @@ package main
 import (
 	"fmt"
 	"goeventify/db"
-	"goeventify/models"
-	"net/http"
-	"strconv"
-
+	"goeventify/routes"
 	"github.com/gin-gonic/gin"
 )
 
@@ -24,53 +21,6 @@ func main(){
 	db.InitDB()
 
 	server := gin.Default()  // default returns an http engine instance with the logger and recovery middleware already attached.
-	server.GET("/events", getEvents)
-	server.GET("/events/:id", getEvent) //dynamic identifier -> /events/1, /events/2
-	server.POST("/events", createEvent)
+	routes.RegisterRoutes(server)
 	server.Run(":8080") //start listening to incoming requests to localhost:8080
-}
-
-func getEvent(context *gin.Context){
-	eventId, err := strconv.ParseInt(context.Param("id"), 10, 64)  //gives parameter as string
-	if err != nil {
-		context.JSON(http.StatusBadRequest, gin.H{"message": "could not parse event id."})
-		return
-	}
-
-	event, err := models.GetEventById(eventId)
-	if err != nil {
-		context.JSON(http.StatusInternalServerError, gin.H{"message": "could not fetch event."})
-		return
-	}
-	context.JSON(http.StatusOK, event)
-}
-
-func getEvents(context *gin.Context)  {
-	events, err := models.GetAllEvents()
-	if err != nil {
-		context.JSON(http.StatusInternalServerError, gin.H{"message":"could not fetch events. Try again later."})
-		return
-	}
-	context.JSON(http.StatusOK, events)   //gin.H is a map (dictionary)
-}
-
-func createEvent(context *gin.Context)  {
-	var event models.Event
-	err := context.ShouldBindJSON(&event)
-
-	if err != nil {
-		context.JSON(http.StatusBadRequest, gin.H{"message": "could not parse data"})
-		return
-	}
-
-	// event.ID = 1
-	// event.UserID = 1
-
-	err = event.Save()
-	if err != nil {
-		fmt.Print(err)
-		context.JSON(http.StatusInternalServerError, gin.H{"message":"could not create event. Try again later."})
-		return
-	}
-	context.JSON(http.StatusOK, gin.H{"message": "Event Created!", "event": event})
 }
