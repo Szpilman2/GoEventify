d07b0c4ff3a0cfce0fef68d7179c378934d1db9c
Peyman Shabani <peymanshabani1998@gmail.com>
2025-03-30 23:24:31 +0330

    refactor code to restructure the project

---
+++ routes/events.go
@@ -0,0 +1,55 @@
+package routes
+
+import (
+	"fmt"
+	"goeventify/models"
+	"net/http"
+	"strconv"
+
+	"github.com/gin-gonic/gin"
+)
+
+func GetEvent(context *gin.Context) {
+	eventId, err := strconv.ParseInt(context.Param("id"), 10, 64) //gives parameter as string
+	if err != nil {
+		context.JSON(http.StatusBadRequest, gin.H{"message": "could not parse event id."})
+		return
+	}
+
+	event, err := models.GetEventById(eventId)
+	if err != nil {
+		context.JSON(http.StatusInternalServerError, gin.H{"message": "could not fetch event."})
+		return
+	}
+	context.JSON(http.StatusOK, event)
+}
+
+func GetEvents(context *gin.Context) {
+	events, err := models.GetAllEvents()
+	if err != nil {
+		context.JSON(http.StatusInternalServerError, gin.H{"message": "could not fetch events. Try again later."})
+		return
+	}
+	context.JSON(http.StatusOK, events) //gin.H is a map (dictionary)
+}
+
+func CreateEvent(context *gin.Context) {
+	var event models.Event
+	err := context.ShouldBindJSON(&event)
+
+	if err != nil {
+		context.JSON(http.StatusBadRequest, gin.H{"message": "could not parse data"})
+		return
+	}
+
+	// event.ID = 1
+	// event.UserID = 1
+
+	err = event.Save()
+	if err != nil {
+		fmt.Print(err)
+		context.JSON(http.StatusInternalServerError, gin.H{"message": "could not create event. Try again later."})
+		return
+	}
+	context.JSON(http.StatusOK, gin.H{"message": "Event Created!", "event": event})
+}
---
+++ routes/routes.go
@@ -0,0 +1,9 @@
+package routes
+
+import "github.com/gin-gonic/gin"
+
+func RegisterRoutes(server *gin.Engine) {
+	server.GET("/events", GetEvents)
+	server.GET("/events/:id", GetEvent) //dynamic identifier -> /events/1, /events/2
+	server.POST("/events", CreateEvent)
+}
