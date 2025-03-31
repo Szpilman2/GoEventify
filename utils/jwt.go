4bc424759026590977217412c367b3f428e33fe6
Peyman Shabani <peymanshabani1998@gmail.com>
2025-03-31 15:44:31 +0330

    add jwt token verification logic

--- utils/jwt.go
+++ utils/jwt.go
@@ -1,6 +1,7 @@
 package utils
 
 import (
+	"errors"
 	"time"
 
 	"github.com/golang-jwt/jwt/v5"
@@ -15,4 +16,30 @@ func GenerateToken(email string, userId int64) (string, error){
 		"exp": time.Now().Add(time.Hour * 2).Unix(),
 	}) //generate new token with data attached to it.
 	return token.SignedString([]byte(secretKey))
+}
+
+func VerifyToken(token string) error {
+	// in golang -> interface{} means any type.
+	// claims -> information in token.
+	parsedToken, err := jwt.Parse(token, func(t *jwt.Token) (interface{}, error) {
+		_, ok := t.Method.(*jwt.SigningMethodHMAC)
+		if !ok {
+			return nil, errors.New("Unexpected signing method")
+		}
+		return secretKey, nil
+	}) //parses the received token and extracts it's information.
+	if err != nil {
+		return errors.New("Could not parse token")
+	}
+	tokenIsValid := parsedToken.Valid
+	if !tokenIsValid{
+		return errors.New("Invalid token")
+	}
+	// claims, ok := parsedToken.Claims(jwt.MapClaims)
+	// if !ok {
+	// 	return errors.New("Invalid token claims")
+	// }
+	// email := claims["email"].(string)
+	// userId := claims["userId"].(int64)
+	return nil
 }
