93f7f6608d471fcc5f991b09147351cbaf806dbe
Peyman Shabani <peymanshabani1998@gmail.com>
2025-03-31 23:12:59 +0330

    add create registrations table code to db

--- db/db.go
+++ db/db.go
@@ -52,4 +52,18 @@ func createTables() {
 	if err != nil {
 		log.Fatalf("could not create events table %v", err)
 	}
+
+	createRegistrationsTable := `
+		CREATE TABLE IF NOT EXISTS registration()
+		id 	INTEGER PRIMARY KEY AUTOINCREMENT,
+		event_id INTEGER,
+		user_id INTEGER,
+		FOREIGN KEY(event_id) REFERENCES events(id),
+		FOREIGN KEY(user_id) REFERENCES users(id)
+	`
+	_, err = DB.Exec(createRegistrationsTable)
+	if err != nil {
+		log.Fatalf("could not create registrations table %v", err)
+	}
+
 }
