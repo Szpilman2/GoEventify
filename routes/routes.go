e48d73a6d5db2a8bebe0f3a1f4f35cc6c921ce98
Peyman Shabani <peymanshabani1998@gmail.com>
2025-03-31 16:33:32 +0330

    add authentication middleware to some routes

--- routes/routes.go
+++ routes/routes.go
@@ -7,11 +7,22 @@ import (
 )
 
 func RegisterRoutes(server *gin.Engine) {
+	//first approach to add middleware: add middleware as second argument like: server.POST("/events", middlewars.Authenticate ,CreateEvent)
+	// server.GET("/events", GetEvents)
+	// server.GET("/events/:id", GetEvent) //dynamic identifier -> /events/1, /events/2
+	// server.POST("/events", middlewars.Authenticate ,CreateEvent)
+	// server.PUT("/events/:id", UpdateEvent)
+	// server.DELETE("/events/:id", DeleteEvent)
+
+	//second way is to create group like below:
 	server.GET("/events", GetEvents)
 	server.GET("/events/:id", GetEvent) //dynamic identifier -> /events/1, /events/2
-	server.POST("/events", middlewars.Authenticate ,CreateEvent)
-	server.PUT("/events/:id", UpdateEvent)
-	server.DELETE("/events/:id", DeleteEvent)
+	authenticated := server.Group("/")
+	authenticated.Use(middlewars.Authenticate)
+	authenticated.POST("/events" ,CreateEvent)
+	authenticated.PUT("/events/:id", UpdateEvent)
+	authenticated.DELETE("/events/:id", DeleteEvent)
+
 	server.POST("/signup", SignUp)
 	server.POST("/login", Login)
 }
